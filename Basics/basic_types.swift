fd399d6cd0d454355fb45000755090a36f58467e
shilpa sharma <shilpa.sharma@stryker.com>
2025-11-20 10:07:38 +0530

    first swift file

---
+++ Basics/basic_types.swift
@@ -0,0 +1,9 @@
+let age: Int = 25
+let height: Double = 5.6
+let weight: Float = 65.5
+let isStudent: Bool = true
+let name: String = "Shilpa"
+let grade: Character = "A"
+
+print(age, height, weight, isStudent, name, grade)
+
